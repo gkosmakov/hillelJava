7b2d8dd2b0f7cbcb24163f8aa57fdc54fad72207
gkosmakov <gleb.kosmakov@gmail.com>
2017-05-16 22:19:05 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -5,23 +5,32 @@ public class Calc {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		int first = scanner.nextInt();
-		int second = scanner.nextInt();
 
-		int sum = first + second;
-		int diff = first - second;
-		int mult = first * second;
-		int div = first / second;
+		int i1 = scanner.nextInt();
+		String operator = scanner.next();
+		int i2 = scanner.nextInt();
 
+		int sum = i1 + i2;
+		int diff = i1 - i2;
+		int mult = i1 * i2;
+		int div = i1 / i2;
+
+		/*
 		System.out.println("Please input first number:");
-		System.out.println("Your input " + first + " as a first number.");
+		System.out.println("Your input " + i1 + " as a first number.");
 		System.out.println("Please input second number:");
-		System.out.println("Your input " + second + " as a second number.");
-		
-		System.out.println("Sum is " + sum);
-		System.out.println("Difference is " + diff);
-		System.out.println("Multiplication is " + mult);
-		System.out.println("Division is " + div);
+		System.out.println("Your input " + i2 + " as a second number.");
+		*/
+
+		if (operator.equals("+")) {
+			System.out.println("Sum is " + sum);
+		} else if (operator.equals("-")) {
+			System.out.println("Difference is " + diff);
+		} else if (operator.equals("*")) {
+			System.out.println("Multiplication is " + mult);
+		} else if (operator.equals("/")) {
+			System.out.println("Division is " + div);
+		}
 
 	}
 }
