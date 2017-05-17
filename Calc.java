f4d4a15e4338e7cfc5cc94ecbedaaa0289b7d55d
gkosmakov <gleb.kosmakov@gmail.com>
2017-05-17 14:39:15 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -5,22 +5,22 @@ public class Calc {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-
+		/*
+		System.out.println("Please input first number:");
+		System.out.println("Your input " + i1 + " as a first number.");
+		*/
 		int i1 = scanner.nextInt();
 		String operator = scanner.next();
+		/*
+		System.out.println("Please input second number:");
+		System.out.println("Your input " + i2 + " as a second number.");
+		*/
 		int i2 = scanner.nextInt();
 
 		int sum = i1 + i2;
 		int diff = i1 - i2;
 		int mult = i1 * i2;
 		int div = i1 / i2;
-
-		/*
-		System.out.println("Please input first number:");
-		System.out.println("Your input " + i1 + " as a first number.");
-		System.out.println("Please input second number:");
-		System.out.println("Your input " + i2 + " as a second number.");
-		*/
 
 		if (operator.equals("+")) {
 			System.out.println("Sum is " + sum);
